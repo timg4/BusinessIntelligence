523fbd13ec6193ef6d10adbfe9a3086cf5ebd6c6
Stefan Merdian <43847479+yamisukii@users.noreply.github.com>
2025-11-04 00:37:07 +0100

    Create a1_check07_integrity.sql for data validation
    Add integrity check for service events and devices

---
+++ post/a1_check07_integrity.sql
@@ -0,0 +1,14 @@
+SET search_path TO dwh_061, stg_061;
+
+WITH joined AS (
+  SELECT COUNT(*) AS joined_rows
+  FROM ft_service_event f
+  JOIN dim_device       dd ON dd.sk_device       = f.sk_device
+  JOIN dim_servicetype  ds ON ds.sk_servicetype  = f.sk_servicetype
+  JOIN dim_technician   dt ON dt.sk_technician   = f.sk_technician
+  JOIN dim_timeday      td ON td.id              = f.day_id
+)
+SELECT
+  joined_rows,
+  CASE WHEN joined_rows = (SELECT COUNT(*) FROM ft_service_event)
+       THEN 'OK' ELSE 'fail' END AS status_check;
