289e29aa34af676c3d89900e8c207c934a9a1bc6
yamisukii <merdian.stefan@gmail.com>
2025-11-30 19:24:11 +0100

    fixed tims failure

--- DWH2/sql/a2_q24_A.sql
+++ DWH2/sql/a2_q24_A.sql
@@ -4,11 +4,11 @@
 SET search_path TO dwh2_061;
 
 SELECT dc.country_name,
-f.recordedvalue_p95 AS p95_recorded_value
+MAX(f.recordedvalue_p95) AS p95_recorded_value
 FROM dwh2_061.ft_param_city_month f
 JOIN dwh2_061.dim_city dc ON f.city_key = dc.city_key
 JOIN dwh2_061.dim_param dp ON f.param_key = dp.param_key
 JOIN dwh2_061.dim_timemonth dt ON f.month_key = dt.month_key
 WHERE dp.param_name = 'CO2' AND dt.year_num = 2024
 GROUP BY dc.country_name
-ORDER BY p95_recorded_value DESC;
+ORDER BY  p95_recorded_value DESC;
